047db4770c69c45aea0f68da314442c53edb91e4
Hugo Tiyoda <hugotiyoda@gmail.com>
2024-03-28 01:24:14 -0300

    deixando os endpoints ja preparados

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type BalanceResponse struct {
 	Total int    `json:"total"`
 	Date  string `json:"balance"`
@@ -23,12 +29,31 @@ type Balance struct {
 }
 
 type TransactionRequest struct {
-	Value       int    `json:"value"`
-	Type        string `json:"type"`
-	Description string `json:"description"`
+	Value       int    `json:"value" binding:"required"`
+	Type        string `json:"type" binding:"required"`
+	Description string `json:"description" binding:"required"`
 }
 
-
 func main() {
+	server := routers()
+	server.Run(":8080")
+}
 
+func routers() *gin.Engine {
+	ginServer := gin.Default()
+	ginServer.GET("/clientes/:id/extrato", func(c *gin.Context) {
+		id := c.Param("id")
+		c.JSON(http.StatusOK, id)
+	})
+
+	ginServer.POST("/clientes/:id/transacoes", func(c *gin.Context) {
+		var request TransactionRequest
+		if err := c.ShouldBindJSON(&request); err != nil {
+			c.JSON(http.StatusUnprocessableEntity, err.Error())
+			return
+		}
+
+	})
+
+	return ginServer
 }
